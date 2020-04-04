00e5a249077b989d3203609156288097bd2fb8d8
Abhishek Sarkar <aksarkar@alum.mit.edu>
2020-04-04 12:46:22 -0400

    Reorganize notebooks

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,12 @@
 ((org-mode . ((org-publish-project-alist .
-                                         (("fqtl-org" . (:base-directory "/project2/mstephens/aksarkar/projects/gtex-fqtl/org"
-                                                                         :publishing-directory "/project2/mstephens/aksarkar/projects/gtex-fqtl/docs"
+                                         (("fqtl-org" . (:base-directory "/broad/compbio/aksarkar/projects/gtex-fqtl/org"
+                                                                         :publishing-directory "/broad/compbio/aksarkar/projects/gtex-fqtl/docs"
                                                                          :publishing-function org-html-publish-to-html
                                                                          :exclude "setup.org"
                                                                          :htmlized-source t
                                                                          ))
-                                          ("fqtl-fig" . (:base-directory "/project2/mstephens/aksarkar/projects/gtex-fqtl/org"
-                                                                         :publishing-directory "/project2/mstephens/aksarkar/projects/gtex-fqtl/docs"
+                                          ("fqtl-fig" . (:base-directory "/broad/compbio/aksarkar/projects/gtex-fqtl/org"
+                                                                         :publishing-directory "/broad/compbio/aksarkar/projects/gtex-fqtl/docs"
                                                                          :publishing-function org-publish-attachment
                                                                          :base-extension "png"
                                                                          :recursive t
